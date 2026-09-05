791054a6b9e10e9e4fefc878d3a79ebd0b8ef0a2
Cheng <yucheng9824@gmail.com>
2026-09-05 22:05:19 +0800

    Complete 08_removeFromArray javascript exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let result = [];
+
+    arr.forEach((item) => {
+        if (!num.includes(item)) {
+            result.push(item);
+        }
+    })
+
+    return result;
 };
 
 // Do not edit below this line
