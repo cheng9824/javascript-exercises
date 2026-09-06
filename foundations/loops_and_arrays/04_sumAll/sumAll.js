f5c7c6bf543f1519cbc818c2891cb3468170e90e
Cheng <yucheng9824@gmail.com>
2026-09-06 09:25:24 +0800

    Complete loops_and_arrays 04_sumAll javascript exercises

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
+    
+    if ((num1 < 0 || num2 < 0) || (!Number.isInteger(num1) || !Number.isInteger(num2))) {
+        return "ERROR";
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            result += i;
+        }
+    } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            result += i;
+        }
+    } else {
+        return "ERROR";
+    }
 
+    return result;
 };
 
 // Do not edit below this line
