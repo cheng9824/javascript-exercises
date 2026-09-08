4e6ef6800026c58341f8dd9d3f88a191a0b868b9
Cheng <yucheng9824@gmail.com>
2026-09-08 14:13:31 +0800

    Complete object_basics 05_findTheOldest javascript exercises

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    const old = arr.reduce((total, item) => {
+        if (!total.yearOfDeath) {
+            total.yearOfDeath = new Date().getFullYear();
+        }
 
+        const totalAge = total.yearOfDeath - total.yearOfBirth;
+        const itemAge = item.yearOfDeath - item.yearOfBirth;
+
+        if (totalAge < itemAge) {
+            return item;
+        } else {
+            return total;
+        }
+    }, arr[0])
+
+    return old;
 };
 
 // Do not edit below this line
