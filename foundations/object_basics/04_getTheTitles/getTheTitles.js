1babb9a934b275e4d017a36bbb3f8851079aa86f
Cheng <yucheng9824@gmail.com>
2026-09-08 11:44:48 +0800

    Complete object_basics 04_getTheTitles javascript exercises

--- foundations/object_basics/04_getTheTitles/getTheTitles.js
+++ foundations/object_basics/04_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+    const result = books.reduce((total, item) => {
+        total.push(item.title);
+        return total;
+    }, []);
 
+    return result;
 };
 
 // Do not edit below this line
