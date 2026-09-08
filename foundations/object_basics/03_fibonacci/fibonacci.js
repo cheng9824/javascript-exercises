58c7f6626694ff7419c18be04222ce0de25083e7
Cheng <yucheng9824@gmail.com>
2026-09-08 11:27:15 +0800

    Complete object_basics fibonacci javascript exercises

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    return num < 0 ? 'OOPS'
+        : num < 1 ? 0
+            : num <= 2 ? 1 : fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
