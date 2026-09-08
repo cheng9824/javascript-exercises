311733e8188f924ba2056bc2b2b7acb2aeb86f42
Cheng <yucheng9824@gmail.com>
2026-09-08 10:46:12 +0800

    Complete object_basics calculator javascript exercises

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -14,16 +14,36 @@ const sum = function (arr) {
   return result;
 };
 
-const multiply = function () {
+const multiply = function (arr) {
+  const result = arr.reduce((total, item) => {
+    return total * item;
+  }, 1)
 
+  return result;
 };
 
-const power = function () {
+const power = function (a, b) {
+  let result = 1;
 
+  for (let i = 0; i < b; i++) {
+    result *= a;
+  }
+
+  return result;
 };
 
-const factorial = function () {
+const factorial = function (num) {
+  let result = 1;
 
+  if (num === 0) {
+    return 1;
+  }
+
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
