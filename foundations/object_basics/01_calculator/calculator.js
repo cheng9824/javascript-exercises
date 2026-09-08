37976c3dcf8f4e8e7d66e1fb70c5fe5a80be8e6d
Cheng <yucheng9824@gmail.com>
2026-09-08 10:12:44 +0800

    Complete add, subtact, sum part of object_basics 01_calculator javascript exercises

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const result = arr.reduce((total, item) => {
+    return total + item;
+  }, 0);
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function () {
 
 };
 
-const power = function() {
-	
+const power = function () {
+
 };
 
-const factorial = function() {
-	
+const factorial = function () {
+
 };
 
 // Do not edit below this line
