c516b3c1382845f68dae899303a07e3a236a2131
Cheng <yucheng9824@gmail.com>
2026-09-08 11:03:32 +0800

    Complete object_basics 02_palindromes exercises

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanStr = str.toLowerCase().replace(/[\W_]/g, '');
 
+    const reverseStr = cleanStr.split('').reverse().join('');
+
+    return cleanStr === reverseStr;
 };
 
 // Do not edit below this line
